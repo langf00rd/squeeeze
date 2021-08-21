bdc328de9c87f3588609e8e3fc8b70be892e4f5f
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2021-08-21 17:13:35 +0700

    Add files via upload

---
+++ precache-manifest.a7537b1220ea10ce13d8857c826a2f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8534f8944b46f8617175",
+    "url": "/css/app.1f84b428.css"
+  },
+  {
+    "revision": "c53fc50553749c60599200d3bba247cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8534f8944b46f8617175",
+    "url": "/js/app.cfbcc704.js"
+  },
+  {
+    "revision": "f2eb4fe19ce298631c5b",
+    "url": "/js/chunk-vendors.20c1abeb.js"
+  },
+  {
+    "revision": "4346e64ffed6b84d5741ac8d66220e48",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
